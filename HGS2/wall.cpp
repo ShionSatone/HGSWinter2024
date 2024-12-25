ff79a57e569a8404afa10ceb9fa6bd27085ecf1f
ShionSatone <221081@st.yoshida-g.ac.jp>
2024-12-25 16:39:01 +0900

    置く壁調整

--- HGS2/wall.cpp
+++ HGS2/wall.cpp
@@ -35,7 +35,7 @@ void InitWall(void)
 	pDevice = GetDevice();
 
 	g_Wall[0].pos = D3DXVECTOR3(0.0f, 0.0f, -130.0f);
-	g_Wall[1].pos = D3DXVECTOR3(0.0f, 0.0f, 200.0f);
+	g_Wall[1].pos = D3DXVECTOR3(0.0f, 0.0f, 130.0f);
 	g_Wall[2].pos = D3DXVECTOR3(300.0f, 0.0f, 0.0f);
 	g_Wall[2].rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 	g_Wall[3].pos = D3DXVECTOR3(-250.0f, 0.0f, 0.0f);
